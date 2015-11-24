ccb9732c36b22f706acbedbb32a827c603c1a7eb
Thomas Komarnicki <komarnicki.thomas@gmail.com>
2015-11-23 22:25:12 -0500

    add song models

--- song_battler/models.py
+++ song_battler/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+
+    url = models.URLField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    name = models.CharField()
+
+
+class SongBattle(models.Model):
+
+    left_song = models.ForeignKey(Song)
+    right_song = models.ForeignKey(Song)
+    winner = models.IntegerField() # 0 = pass, 1 = left, 2 = right
+    user = models.ForeignKey # todo
